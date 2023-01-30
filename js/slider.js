59a66d03d686620cfeabbdb881bab0f0dc8cfadb
valerie2107 <kinkamyl@hotmail.com>
2023-01-30 14:16:14 +0100

    photos galerie ok, cha,gement de longueur des hr et de couleurs pour les  rdv et Btt, suppression du JS pour le slider car en conflit avec le css

--- js/slider.js
+++ js/slider.js
@@ -12,15 +12,24 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("custom-slider");
+  console.log(slides);
+
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  console.log(dots);
+
+  if (n > slides.length) {slideIndex = 1} 
+
   if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";  
+
   dots[slideIndex-1].className += " active";
 }
